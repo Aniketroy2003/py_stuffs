4805b12934f3ffa3be5208846df3d3d8bfbb3924
Aniket Roy <73846184+Aniketroy2003@users.noreply.github.com>
2022-08-15 02:55:49 +0530

    Create NumberRightPyramid.py

---
+++ NumberRightPyramid.py
@@ -0,0 +1,17 @@
+a = 5
+b = 1
+for i in range(0,a):
+    for j in range(0,i+1):
+        print(b,end=" ")
+        b+=1
+        
+        
+    print("")
+    
+    
+    
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
