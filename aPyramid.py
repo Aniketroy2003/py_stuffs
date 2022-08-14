c3a6e3a5c3b342bf8a362c072a4ecb9880a35b80
Aniket Roy <73846184+Aniketroy2003@users.noreply.github.com>
2022-08-15 02:47:12 +0530

    Create aPyramid.py

---
+++ aPyramid.py
@@ -0,0 +1,20 @@
+a = 5
+b = a-1
+for i in range(0,a):
+    for j in range(0,b):
+        print(end=" ")
+        
+    b = b-1
+    
+    for k in range(0,i+1):
+        print("* ",end='')
+        
+    print('')
+  
+  
+  
+#       *
+#      * *  
+#     * * *   
+#    * * * *  
+#   * * * * *
